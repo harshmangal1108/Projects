ff5ec11e649691eb9f97bc86265eed50b7a44bac
Harsh Mangal <51486214+harshmangal1108@users.noreply.github.com>
2020-05-03 18:28:54 +0530

    Add files via upload

--- DS_webapp/app.py
+++ DS_webapp/app.py
@@ -17,7 +17,8 @@ def load_data(nrows):
 
 
 ## calling funcn and loading only 100000 
-data=load_data(10000)
+data=load_data(20000)
+original_data=data
 st.header("Where are most people injured in NYC")
 injured=st.slider("Number of Persons injured",0,19)
 st.map(data.query("injured_persons >=@injured")[["latitude","longitude"]].dropna(how="any"))
@@ -59,6 +60,17 @@ hist=np.histogram(filtered['date/time'].dt.minute,bins=60,range=(0,60))[0]
 chart_data=pd.DataFrame({'minute':range(60),'crashes':hist})
 fig=px.bar(chart_data,x='minute',y='crashes',hover_data=['minute','crashes'],height=400)
 st.write(fig)
+
+st.header("Top 5 dangerous streets by affected type ")
+select=st.selectbox('Affected type of People',['Pedestrains','Cyclists','Motorists'])
+if select == 'Pedestrians':
+    st.write(original_data.query("injured_pedestrians >= 1")[["on_street_name", "injured_pedestrians"]].sort_values(by=['injured_pedestrians'], ascending=False).dropna(how="any")[:5])
+
+elif select == 'Cyclists':
+    st.write(original_data.query("injured_cyclists >= 1")[["on_street_name", "injured_cyclists"]].sort_values(by=['injured_cyclists'], ascending=False).dropna(how="any")[:5])
+
+else:
+    st.write(original_data.query("injured_motorists >= 1")[["on_street_name", "injured_motorists"]].sort_values(by=['injured_motorists'], ascending=False).dropna(how="any")[:5])
 ## write data
 if st.checkbox("show data ",False):
     st.subheader('RAW DATA')
